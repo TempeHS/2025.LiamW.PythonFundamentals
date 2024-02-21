500bdcf8e420f9aae00129b59b90d607cb62ad16
WuMail24 <158523770+WuMail24@users.noreply.github.com>
2024-02-21 23:10:52 +0000

    Upload completed camel.py

--- 2-Loops/camel/camel.py
+++ 2-Loops/camel/camel.py
@@ -5,7 +5,12 @@ def main():
 
 def snake(case):
     for letter in case:
-        print(letter, end=" ")
+        if letter.isupper():
+            letter = letter.lower()
+            print(f"_{letter}", end="")
+        else:
+            print(letter, end="")
+    print("")
 
 
 main()
