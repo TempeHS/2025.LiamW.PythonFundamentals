2c4c6b6e14ac6f5934ec830c87006bacc997d677
WuMail24 <158523770+WuMail24@users.noreply.github.com>
2024-02-23 00:54:04 +0000

    Upload completed coke.py

--- 2-Loops/coke/coke.py
+++ 2-Loops/coke/coke.py
@@ -2,9 +2,12 @@ def main():
     remain = 50
     while remain > 0:
         amount = input("Insert Coin 10, 25 or 5! ")
-        if amount == "25" or "10" or "5":
+        if amount == "25" or amount == "10" or amount == "5":
             amount = int(amount)
             remain = int(remain) - amount
+            if remain > 0:
+                print(f"Amount Due: {remain}")
+        else:
             print(f"Amount Due: {remain}")
     if remain <= 0:
         remainder = str(remain).strip("-")
