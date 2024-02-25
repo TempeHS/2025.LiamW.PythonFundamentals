5d8ec366d507ab2f1818cd75ff766e3ab66fe1e2
WuMail24 <158523770+WuMail24@users.noreply.github.com>
2024-02-25 22:39:05 +0000

    incomplete nutrition.py

---
+++ 2-Loops/nutrition/nutrition.py
@@ -0,0 +1,13 @@
+fruit = input("Fruit: ")
+Fruits = {
+    "Avocado"
+    | "Cantaloupe"
+    | "Honeydew Melon"
+    | "Pineapple"
+    | "Strawberries"
+    | "Tangerine": "50",
+    "Grapefruit" | "Nectarine" | "Peach": "60",
+    "Plum": "70",
+    "Orange" | "Watermelon": "80",
+    "Grapes" | "Kiwifruit": "90",
+}
