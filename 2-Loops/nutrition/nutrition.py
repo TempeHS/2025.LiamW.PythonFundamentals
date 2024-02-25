693bbdc9e4de384f026a77db8f73663251af17a9
WuMail24 <158523770+WuMail24@users.noreply.github.com>
2024-02-25 23:30:15 +0000

    Upload complete nutrition.py

--- 2-Loops/nutrition/nutrition.py
+++ 2-Loops/nutrition/nutrition.py
@@ -1,13 +1,26 @@
 fruit = input("Fruit: ")
+fruit = fruit.strip().lower()
 Fruits = {
-    "Avocado"
-    | "Cantaloupe"
-    | "Honeydew Melon"
-    | "Pineapple"
-    | "Strawberries"
-    | "Tangerine": "50",
-    "Grapefruit" | "Nectarine" | "Peach": "60",
-    "Plum": "70",
-    "Orange" | "Watermelon": "80",
-    "Grapes" | "Kiwifruit": "90",
+    "lemon": "15",
+    "lime": "20",
+    "avocado": "50",
+    "cantaloupe": "50",
+    "honeydew melon": "50",
+    "pineapple": "50",
+    "strawberries": "50",
+    "tangerine": "50",
+    "grapefruit": "60",
+    "nectarine": "60",
+    "peach": "60",
+    "plum": "70",
+    "orange": "80",
+    "watermelon": "80",
+    "grapes": "90",
+    "kiwifruit": "90",
+    "pear": "100",
+    "sweet cherries": "100",
+    "banana": "110",
+    "apple": "130",
 }
+if fruit in Fruits:
+    print(f"Calories: {Fruits[fruit]}")
