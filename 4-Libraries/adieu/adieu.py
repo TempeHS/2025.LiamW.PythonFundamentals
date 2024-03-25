88e57f019f13a6df0df43b68abac7e56a4abf5fb
WuMail24 <158523770+WuMail24@users.noreply.github.com>
2024-03-25 11:03:10 +0000

    Upload complete adieu.py

--- 4-Libraries/adieu/adieu.py
+++ 4-Libraries/adieu/adieu.py
@@ -1,1 +1,21 @@
 import inflect
+
+p = inflect.engine()
+n = 1
+namelist = []
+while True:
+    try:
+        namelist.append(input("Name: "))
+    except EOFError:
+        print("")
+        break
+
+print("Adieu, adieu, to ", end="")
+for name in namelist:
+    n = n + 1
+    if len(namelist) == 1:
+        print(f"{name}")
+    elif n < len(namelist) + 1:
+        print(f"{name}, ", end="")
+    else:
+        print(f"and {name}")
