cb9903306d506e68d309f298b102dcec6240642d
WuMail24 <158523770+WuMail24@users.noreply.github.com>
2024-05-24 04:06:30 +0000

    need to finish setloc and getcol

--- class_lesson/ooptask/ooptask.py
+++ class_lesson/ooptask/ooptask.py
@@ -8,6 +8,12 @@ class Shape:
     def __str__(self):
         return self.__name__
 
+    def getlocation(self):
+        return f"({self.x},{self.y},{self.z})"
+
+    def setlocation(self): ...
+    def getcolour(self): ...
+
 
 class Cube(Shape):
     def __init__(self, colour, x, y, z, length):
@@ -31,8 +37,30 @@ class Cube(Shape):
         return face
 
 
+class Sphere(Shape):
+    def __init__(self, colour, x, y, z, radius):
+        super().__init__(colour, x, y, z)
+        self.radius = radius
+
+    def __str__(self):
+        return super().__str__()
+
+    def Volume(self):
+        Vol = (4 / 3) * 3.14 * (self.radius**3)
+        return Vol
+
+    def SA(self):
+        SurfaceA = 4 * 3.14 * (self.radius**2)
+        return SurfaceA
+
+    def crossSec(self):
+        face = 3.14 * (self.radius**2)
+        return face
+
+
 def main():
     cube = Cube("blue", 0, 0, 0, 3)
+    print(cube.getlocation())
     print(cube.Volume())
 
 
